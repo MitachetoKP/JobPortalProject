7798b52c5d5cdc48151e9d3f43d70ce623591ab9
Mitaka <namita4eto@gmail.com>
2022-12-07 10:31:54 +0200

    Replaced admin area with employer area

---
+++ JobPortalProject/Areas/Employer/Controllers/EmployerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobPortalProject.Areas.Employer.Controllers
+{
+    public class EmployerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
