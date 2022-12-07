0917f2e02ba5c446774100d3d42ffeb35118d8b0
Mitaka <namita4eto@gmail.com>
2022-12-07 10:32:10 +0200

    Remade employer table

--- JobPortalProject.Infrastructure/Data/Entities/Employer.cs
+++ JobPortalProject.Infrastructure/Data/Entities/Employer.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JobPortalProject.Infrastructure.Data.Entities
 {
@@ -16,17 +12,26 @@ namespace JobPortalProject.Infrastructure.Data.Entities
         [MaxLength(50)]
         public string Name { get; set; }
 
-        [Required]
-        [MaxLength(100)]
-        [EmailAddress]
-        public string Email { get; set; }
+        //[Required]
+        //[MaxLength(100)]
+        //[EmailAddress]
+        //public string Email { get; set; }
+
+        //[Required]
+        //[MaxLength(32)]
+        //public string Password { get; set; }
+
+        //[Required]
+        //public string LogoUrl { get; set; }
 
         [Required]
-        [MaxLength(32)]
-        public string Password { get; set; }
+        [MaxLength(15)]
+        [Phone]
+        public string PhoneNumber { get; set; }
 
-        [Required]
-        public string LogoUrl { get; set; }
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+        public Employee User { get; set; }
 
         public IEnumerable<Offer> MyOffers { get; set; } = new List<Offer>();
     }
---
+++ JobPortalProject.Infrastructure/Migrations/20221207082046_EmployerTable.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobPortalProject.Infrastructure.Migrations
+{
+    public partial class EmployerTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Employers");
+
+            migrationBuilder.DropColumn(
+                name: "LogoUrl",
+                table: "Employers");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Employers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Employers",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Employers",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employers_UserId",
+                table: "Employers",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employers_AspNetUsers_UserId",
+                table: "Employers",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employers_AspNetUsers_UserId",
+                table: "Employers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employers_UserId",
+                table: "Employers");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Employers");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Employers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Employers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LogoUrl",
+                table: "Employers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Employers",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
